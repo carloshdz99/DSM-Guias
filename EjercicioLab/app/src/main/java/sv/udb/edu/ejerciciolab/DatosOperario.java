a2b07668f99c84d2cc3a380e5c916b6ec39bd8b9
carloshdz99 <carloshdzfunes@gmail.com>
2021-02-19 23:05:35 -0600

    Pasando datos a segunda activity

--- EjercicioLab/app/src/main/java/sv/udb/edu/ejerciciolab/DatosOperario.java
+++ EjercicioLab/app/src/main/java/sv/udb/edu/ejerciciolab/DatosOperario.java
@@ -10,9 +10,29 @@ public class DatosOperario extends AppCompatActivity {
 
     private TextView tvSueldo;
     private TextView tvAños;
+    private TextView tvaumento;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_datos_operario);
+
+        Bundle bundle = getIntent().getExtras();
+        String Nsueldo = bundle.getString("sueldo");
+        String Naños = bundle.getString("años");
+        String Naumento = bundle.getString("aumento");
+
+        Toast notification = Toast.makeText(this, "El sueldo del operario es: "+Nsueldo+"", Toast.LENGTH_LONG);
+        notification.show();
+        Toast notification2 = Toast.makeText(this, "Los años del operario son del operario es: "+Naños+"", Toast.LENGTH_LONG);
+        notification2.show();
+
+        //asignando textos a textview
+        tvSueldo = (TextView)findViewById(R.id.datoSueldo);
+        tvSueldo.setText(Nsueldo);
+        tvaumento = (TextView)findViewById(R.id.datoAumento);
+        tvaumento.setText(Naumento);
+        tvAños = (TextView)findViewById(R.id.datoAños);
+        tvAños.setText(Naños);
+
     }
 }
--- EjercicioLab/app/src/main/java/sv/udb/edu/ejerciciolab/MainActivity.java
+++ EjercicioLab/app/src/main/java/sv/udb/edu/ejerciciolab/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    //declarando variables
     private EditText sueldo;
     private EditText años;
     private Button btncalcular;
@@ -18,13 +19,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //tomando los campos a ingresar y el boton por su id
         sueldo=(EditText)findViewById(R.id.nSueldo);
         años = (EditText)findViewById(R.id.nAños);
         btncalcular = findViewById(R.id.btncalcular);
 
+        //evento del boton que calculara el sueldo
         btncalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
+                //realizando conversion
+               calcular(view);
 
             }
         });
@@ -32,9 +37,26 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void calcular(View view){
-        Intent i = new Intent(this, DatosOperario.class);
-        i.putExtra("sueldo", sueldo.getText().toString());
-        i.putExtra("años", años.getText().toString());
-        startActivity(i);
+        //realizando conversion
+                int Naños = Integer.parseInt(años.getText().toString());
+                double Nsueldo = Double.parseDouble(sueldo.getText().toString());
+                String aumento = "";
+
+                //validaciones de sueldo
+                if((Nsueldo > 100 && Nsueldo < 500) && Naños >= 10 ){
+                    Nsueldo = Nsueldo + (Nsueldo*0.2);
+                    aumento = "20%";
+                }else if((Nsueldo > 100 && Nsueldo < 500) && Naños < 10){
+                    Nsueldo = Nsueldo + (Nsueldo*0.05);
+                    aumento = "5%";
+                }else if(Nsueldo >= 500){
+                    aumento = "0%";
+                }
+
+                Intent i = new Intent(this, DatosOperario.class);
+                i.putExtra("sueldo", String.valueOf(Nsueldo));
+                i.putExtra("años", String.valueOf(Naños));
+                i.putExtra("aumento", aumento);
+                startActivity(i);
     }
 }
