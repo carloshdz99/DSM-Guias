dba2df0baa95dafa65c08fb2d4e3873bb45a2bb2
carloshdz99 <carloshdzfunes@gmail.com>
2021-04-11 22:05:41 -0600

    listview

--- BeautyAppDsm/app/src/main/java/com/beautyapp/ui/slideshow/SlideshowFragment.java
+++ BeautyAppDsm/app/src/main/java/com/beautyapp/ui/slideshow/SlideshowFragment.java
@@ -1,9 +1,12 @@
 package com.beautyapp.ui.slideshow;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,10 +16,24 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.beautyapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class SlideshowFragment extends Fragment {
 
     private SlideshowViewModel slideshowViewModel;
+
+    private ListView listView;
+    private ArrayList<String> arrayList;
+    private ArrayAdapter<String> arrayAdapter;
+
+    private FirebaseAuth mAuth;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,6 +41,50 @@ public class SlideshowFragment extends Fragment {
                 new ViewModelProvider(this).get(SlideshowViewModel.class);
         View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
 
+        //tomando lista
+        arrayList = new ArrayList<String>();
+
+
+
+        //instanciando firebase
+        mAuth = FirebaseAuth.getInstance();
+
+        //tomando la listview
+        listView = root.findViewById(R.id.ltsCitas);
+
+        //adaptador de arreglo
+       // arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, arrayList);
+
+        getCitas();
+        //pasando datos a la listview
+        //listView.setAdapter(arrayAdapter);
+
+
         return root;
+    }
+
+
+    private void getCitas(){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        //tomando las citas
+        db.collection("citas")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    private static final String TAG ="firestore" ;
+
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()){
+                            for (QueryDocumentSnapshot document : task.getResult()){
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                arrayList.add(document.getData().toString());
+                                arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, arrayList);
+                                listView.setAdapter(arrayAdapter);
+                            }
+                        }else {
+                            Log.w(TAG, "error al llamar los datos ", task.getException());
+                        }
+                    }
+                });
     }
 }
